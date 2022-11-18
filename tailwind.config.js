0326a7f6fc5e74bb6852512ea0b8154d2f855ed9
Phillip Lam <81057116+philliplam8@users.noreply.github.com>
2022-11-18 16:43:14 +0000

    style: adding mockup design colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'primary': 'hsl(180, 29%, 50%)',
+      'background': 'hsl(180, 52%, 96%)',
+      'lightGrayishCyan': 'hsl(180, 31%, 95%)',
+      'darkGrayishCyan': 'hsl(180, 8%, 52%)',
+      'veryDarkGrayishCyan': 'hsl(180, 14%, 20%)'
+    },
     extend: {},
   },
   plugins: [],
