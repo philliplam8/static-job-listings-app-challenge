fc261773aa2eb1dc95253eccb67e458b89a32ac4
Phillip <phillip.lam0@gmail.com>
2022-11-18 10:11:53 -0500

    feat: configuring font with NextJS Document page

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.tsx
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="light:bg-white light:text-black dark:bg-black dark:text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
