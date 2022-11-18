b4c96af9c8c63cd8a128da06ac153dfb7ac759ed
Phillip <phillip.lam0@gmail.com>
2022-11-18 18:19:12 -0500

    fix(tag): updated element to be a button

--- components/TagGroup/Tag.tsx
+++ components/TagGroup/Tag.tsx
@@ -1,7 +1,7 @@
 export default function Tag(props: { label: string }): JSX.Element {
   return (
-    <div className="mt-4 md:mt-0 py-1 px-2 bg-lightGrayishCyan rounded-md">
-      <h3 className="text-primary text-sm font-bold">{props.label}</h3>
-    </div>
+    <button className="mt-4 md:mt-0 py-1 px-2 bg-lightGrayishCyan hover:bg-primary active:bg-primary text-primary hover:text-white active:text-white rounded-md">
+      <h3 className=" text-sm font-bold">{props.label}</h3>
+    </button>
   );
 }
