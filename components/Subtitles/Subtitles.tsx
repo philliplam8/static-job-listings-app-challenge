6efc27750aa67e75ed71e9e77a46d12e7eff2333
Phillip <phillip.lam0@gmail.com>
2022-11-18 18:17:05 -0500

    refactor(subtitles): abstracted job postedAt, contract, and locationd details

---
+++ components/Subtitles/Subtitles.tsx
@@ -0,0 +1,17 @@
+interface SubtitleType {
+  postedAt: string;
+  contract: string;
+  location: string;
+}
+
+export default function Subtitles(props: SubtitleType): JSX.Element {
+  return (
+    <div className="flex flex-row gap-2 text-darkGrayishCyan">
+      <p>{props.postedAt}</p>
+      <p>•</p>
+      <p>{props.contract}</p>
+      <p>•</p>
+      <p>{props.location}</p>
+    </div>
+  );
+}
---
+++ components/Subtitles/index.ts
@@ -0,0 +1,3 @@
+import Subtitles from "./Subtitles";
+
+export { Subtitles };
