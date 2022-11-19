3e317d3fe385ab28e43d98c5d18a032a3cc37199
Phillip <phillip.lam0@gmail.com>
2022-11-19 16:28:03 -0500

    refactor(filter context): moving filter handler functions to context

--- components/TagGroup/Tag.tsx
+++ components/TagGroup/Tag.tsx
@@ -1,28 +1,8 @@
-import cloneDeep from "lodash/cloneDeep";
 import { useContext } from "react";
 import { FilterContext } from "../../context/FilterContext";
 
 export default function Tag(props: { label: string }): JSX.Element {
-  const [filters, setFilters] = useContext(FilterContext);
-
-  const addFilter = (filter: string): void => {
-    let tempFilters = cloneDeep(filters);
-    tempFilters[filter] = true;
-    setFilters(tempFilters);
-  };
-
-  const removeFilter = (filter: string): void => {
-    let tempFilters = cloneDeep(filters);
-    // Check if filter key exists in filters object
-    if (!(filter in tempFilters)) {
-      return;
-    }
-    // Toggle key value to be false to "remove" filter
-    else {
-      tempFilters[filter] = false;
-    }
-    setFilters(tempFilters);
-  };
+  const [filters, addFilter, removeFilter] = useContext(FilterContext);
 
   return (
     <button
--- context/FilterContext.js
+++ context/FilterContext.js
@@ -1,13 +1,33 @@
 import { createContext, useState } from "react";
+import cloneDeep from "lodash/cloneDeep";
 
 export const FilterContext = createContext();
 
 export const FilterProvider = ({ children }) => {
   const [filters, setFilters] = useState({});
 
+  const addFilter = (filter) => {
+    let tempFilters = cloneDeep(filters);
+    tempFilters[filter] = true;
+    setFilters(tempFilters);
+  };
+
+  const removeFilter = (filter) => {
+    let tempFilters = cloneDeep(filters);
+    // Check if filter key exists in filters object
+    if (!(filter in tempFilters)) {
+      return;
+    }
+    // Toggle key value to be false to "remove" filter
+    else {
+      tempFilters[filter] = false;
+    }
+    setFilters(tempFilters);
+  };
+
   return (
     <FilterContext.Provider
-      value={[filters, setFilters]}
+      value={[filters, addFilter, removeFilter]}
     >
       {children}
     </FilterContext.Provider>
