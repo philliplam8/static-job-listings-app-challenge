6fb829a0c742d29eda5e072957df8a12eba1af71
Phillip <phillip.lam0@gmail.com>
2022-11-19 21:00:53 -0500

    style(tag): increasing padding

--- components/TagGroup/Tag.tsx
+++ components/TagGroup/Tag.tsx
@@ -6,7 +6,7 @@ export default function Tag(props: { label: string }): JSX.Element {
 
   return (
     <button
-      className="mt-4 md:mt-0 py-1 px-2 bg-lightGrayishCyan hover:bg-primary active:bg-primary text-primary hover:text-white active:text-white rounded-md"
+      className="mt-4 md:mt-0 py-1.5 px-2 bg-lightGrayishCyan hover:bg-primary active:bg-primary text-primary hover:text-white active:text-white rounded-md"
       onClick={() => addFilter(props.label)}
     >
       <h3 className=" text-sm font-bold">{props.label}</h3>
