6caa67028590a1e5a8b52246eaa24c6f50635870
Phillip <phillip.lam0@gmail.com>
2022-11-21 12:13:39 -0500

    docs(footer): updating types

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,17 +1,22 @@
-export default function Footer(): JSX.Element {
-  function Link(props: { label: string; link: string }) {
-    return (
-      <a
-        href={props.link}
-        target="_blank"
-        rel="noreferrer"
-        className="font-bold hover:text-primary hover:underline active:text-primary active:underline"
-      >
-        {props.label}
-      </a>
-    );
-  }
+interface LinkProps {
+  label: string;
+  link: string;
+}
 
+function Link(props: LinkProps): JSX.Element {
+  return (
+    <a
+      href={props.link}
+      target="_blank"
+      rel="noreferrer"
+      className="font-bold hover:text-primary hover:underline active:text-primary active:underline"
+    >
+      {props.label}
+    </a>
+  );
+}
+
+export default function Footer(): JSX.Element {
   return (
     <footer className="flex flex-row justify-center py-5 text-xs">
       <p>
