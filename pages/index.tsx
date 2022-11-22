06c230fd4bd56d176fb70caf790765501834d73a
Phillip <phillip.lam0@gmail.com>
2022-11-22 15:39:59 -0500

    feat(index): wrapping error in Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,24 @@
-import { useRouter } from "next/router";
 import useSWR from "swr";
 import { Layout } from "../components/Layout";
 import { Jobs } from "../components/Job";
 import { AppliedFilters } from "../components/Filter";
 import { SkeletonLoader } from "../components/SkeletonLoader";
 
-// Fetcher functop to wrap the native fetch function and return the result of a call to url in json format
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
 export default function Home() {
-  const router = useRouter();
+  // Fetcher functop to wrap the native fetch function and return the result of a call to url in json format
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
   // Set up SWR to run the fetch function when calling '/api/staticdata'
   // There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
   const { data, error } = useSWR("/api/staticdata", fetcher);
 
   // Handle the error state
-  if (error) return <div>Failed to load</div>;
-  // if (error) {
-  //   router.push("/500");
-  // }
+  if (error)
+    return (
+      <Layout>
+        <div className="my-8">Failed to load jobs</div>
+      </Layout>
+    );
 
   // Handle the loading state and the ready state and display the result contained in the data object
   // mapped to the structure of the json file
