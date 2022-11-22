1b74efb2455ff0719c4b8c5789912550617fdbdc
Phillip <phillip.lam0@gmail.com>
2022-11-21 19:16:28 -0500

    fix(tag): h2 should not be allowed as a child of a button

--- components/TagGroup/Tag.tsx
+++ components/TagGroup/Tag.tsx
@@ -9,10 +9,10 @@ export default function Tag(props: {
 
   return (
     <button
-      className="mt-4 md:mt-0 py-1.5 px-2 bg-lightGrayishCyan hover:bg-primary active:bg-primary text-primary hover:text-white active:text-white rounded-md"
+      className="mt-4 md:mt-0 py-1.5 px-2 bg-lightGrayishCyan hover:bg-primary active:bg-primary text-primary hover:text-white active:text-white rounded-md text-md font-bold"
       onClick={() => addFilter(props.label, props.labelType)}
     >
-      <h2 className="text-md font-bold">{props.label}</h2>
+      {props.label}
     </button>
   );
 }
