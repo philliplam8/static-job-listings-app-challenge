f15e61a9687dea7c7ab3b3b0eaef315d3065e3f2
Phillip <phillip.lam0@gmail.com>
2022-11-22 16:54:44 -0500

    fix(avatar): use regular img tag instead of NextJS Image tag

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 
 export default function Avatar(props: { logo: string }): JSX.Element {
   return (
     <div className="w-14 h-14 md:w-[89px] md:h-full mt-2 md:mt-0 -top-[70px] md:top-0 absolute md:relative rounded-full object-fit">
-      <Image src={props.logo} alt={""} width={100} height={100} />
+      <img src={props.logo} alt={""} width={100} height={100} />
     </div>
   );
 }
